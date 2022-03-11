8526fa60225aa0139e78146216afd0e0887d661a
njeans <nerlajeanlouis@gmail.com>
2022-03-11 14:45:10 -0600

    update gitignore

--- run_sim.sh
+++ run_sim.sh
@@ -1,7 +1,7 @@
 curr=$PWD
 # echo $curr
-#docker run --rm -v $PWD/pktransfer:/root/sgx/samplecode/pktransfer --env GANACHEIP="$GANACHEIP"  -ti pktransferenclave
-docker run --rm --env GANACHEIP="$GANACHEIP" -ti pktransferenclave
+docker run --rm -v $PWD/pktransfer:/root/sgx/samplecode/pktransfer --env GANACHEIP="$GANACHEIP"  -ti pktransferenclave
+# docker run --rm --env GANACHEIP="$GANACHEIP" -ti pktransferenclave
 
 # Run ganache server
 # docker run --name pktransferganache -it --publish 8545:8545 trufflesuite/ganache-cli:latest --accounts 10 --debug
